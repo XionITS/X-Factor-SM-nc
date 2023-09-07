d186714d5996d6308d81d67ac169a8cf8318e3d4
dongzen <krtdj@naver.com>
2023-09-07 21:40:25 +0900

    11

--- static/assets/js/ncsm/his_asset.js
+++ static/assets/js/ncsm/his_asset.js
@@ -292,11 +292,90 @@ searchButton.addEventListener('click', function() {
             searchText: inputValue
         },
         success: function(res) {
-            var data = res.data;
+            var data = res.data[0]; // 첫 번째 객체 선택
             console.log(data);
+            console.log(data.disk_use);
+
+
+            var valueMap = {
+                '사용자': data.computer.computer_name,
+                '메모': data.computer.memo,
+                'IP 주소': data.computer.ip_address,
+                'Mac 주소': data.computer.mac_address,
+                'OS 종류': data.computer.os_simple,
+                'OS 버전': data.computer.os_version,
+                'Office 365 버전': data.essential2,
+                '메모리 사용량': data.mem_use,
+                '디스크 사용량': data.disk_use,
+                '최초 네트워크 접속일': data.first_network,
+                '내 컴퓨터 정보' :data.hw_cpu
+            };
+
+
+            // 각 <td> 요소에 값 설정
+            var ipAddressElement = document.getElementById("asset_ip_address");
+            if (ipAddressElement) {
+              ipAddressElement.textContent = valueMap['IP 주소'];
+            }
+            var computerNameElement = document.getElementById("asset_computer_name");
+            if (computerNameElement) {
+              computerNameElement.textContent = data.computer.computer_name;
+            }
+
+            var memoElement = document.getElementById("asset_memo");
+            if (memoElement) {
+              memoElement.textContent = data.computer.memo || "";
+            }
+
+            var ipAddressElement = document.getElementById("asset_ip_address");
+            if (ipAddressElement) {
+              ipAddressElement.textContent = data.computer.ip_address;
+            }
+            var macAddressElement = document.getElementById("asset_mac_address");
+            if (macAddressElement) {
+             macAddressElement.textContent = data.computer.mac_address;
+            }
+
+            var osTypeElement = document.getElementById("asset_os_simple");
+            if (osTypeElement) {
+             osTypeElement.textContent = data.computer.os_simple;
+            }
+
+            var osVersionElement = document.getElementById("asset_os_version");
+            if (osVersionElement) {
+             osVersionElement.textContent = data.computer.os_version;
+            }
+
+            var office365VersionElement= document.getElementById("asset_office_version");
+            if (office365VersionElement) {
+             office365VersionElement.textContent= data.essential2 || "";
+            }
+
+            var memoryUsageElement=document.getElementById('asset_mem_use');
+            if(memoryUsageElement){
+            memoryUsageElement.textContent=valueMap['메모리 사용량']||"";
+            }
+
+            var diskUsageElement=document.getElementById('asset_disk_use');
+            if(diskUsageElement){
+            diskUsageElement.textContent=valueMap['디스크 사용량']||"";
+            }
+
+            var firstNetworkAccessDateElement=document.getElementById('asset_first_network');
+            if(firstNetworkAccessDateElement){
+            firstNetworkAccessDateElement.textContent=data.first_network||"";
+            }
+
+
+            var myComputerInfoElemnt=document.getElementById('asset_hw_cpu');
+            if(myComputerInfoElemnt){
+             myComputerInfoElemnt.innerText="CPU: "+data.computer.hw_cpu + " \n RAM : "+data.computer.hw_ram+ " \n 메인보드 : "+data.computer.hw_mb+ " \n 디스크 : "+data.computer.hw_disk+ " \n 그래픽카드 : "+data.computer.hw_vga;
+           }
+
         }
     });
 });
+
 
 
 $(document).ready(function () {
