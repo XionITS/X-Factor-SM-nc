3a40779a8b889014f03e32ad465f97717cd75445
skchoi@xionits.com <8285sksk!!>
2023-10-20 18:13:46 +0900

    리포트 페이지 레이아웃 및 스타일 작성, 대시보드 월별 자산수 시간대별 대시보드 구현, 리포트 팝업 구현, pdf생성 라이브러리 추가, NC로고 추가, 시간대별 리포트 생성 데이터 통계

--- static/assets/js/ncsm/report_dailyChart.js
+++ static/assets/js/ncsm/report_dailyChart.js
@@ -4,9 +4,17 @@ Version: 1.8.0
 Author: Sean Ngu
 Website: http://www.seantheme.com/hud/
 */
+
+//세자리 숫자마다 ,붙여주는 함수
 function numberWithCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 };
+
+//null 값을 0으로 변환하는 함수
+function parseValue(value) {
+    return value === "null" ? 0 : value;
+}
+
 var handleRenderdailyApexChart = function () {
 	Apex = {
 		title: {
@@ -81,26 +89,125 @@ var handleRenderdailyApexChart = function () {
 	};
 
 // 장기 미접속 자산 증가율
+var dataFor150days = dataList['150days'];
+var currentDataValue = parseValue(dataFor150days['current_value']);
+var lastMonthDataValue = parseValue(dataFor150days['last_value_in_prev_month']);
 var options150days = {
     chart: {
         type: 'bar',
-        height: 350
+        height: 200
     },
     series: [{
         name: '전월 자산 수',
-        data: [dataList['150days']['Previous data'] === "None" ? 0 : dataList['150days']['Previous data']]
+        data: [lastMonthDataValue]
     }, {
         name: '현재 자산 수',
-        data: [dataList['150days']['Current data']]
+        data: [currentDataValue]
     }],
     xaxis: {
         categories: ['150days']
     }
 }
-
-
 var chart150days = new ApexCharts(document.querySelector("#chart-150days"), options150days);
 chart150days.render();
+
+// 업데이트 대상 수 변화량
+var dataFor_osVersionUp = dataList['os_version_up'];
+var currentDataValue = parseValue(dataFor_osVersionUp['current_value']);
+var lastMonthDataValue = parseValue(dataFor_osVersionUp['last_value_in_prev_month']);
+var options_osVersionUp = {
+    chart: {
+        type: 'bar',
+        height: 200
+    },
+    series: [{
+        name: '전월 자산 수',
+        data: [lastMonthDataValue]
+    }, {
+        name: '현재 자산 수',
+        data: [currentDataValue]
+    }],
+    xaxis: {
+        categories: ['os_version_up']
+    }
+}
+var chart_os_version_up = new ApexCharts(document.querySelector("#chart_os_version_up"), options_osVersionUp);
+chart_os_version_up.render();
+
+// 보안 패치 대상 수 변화량
+var dataFor_hotfix = dataList['hotfix'];
+var currentDataValue_hotfix = parseValue(dataFor_hotfix['current_value']);
+var lastMonthDataValue_hotfix = parseValue(dataFor_hotfix['last_value_in_prev_month']);
+var options_hotfix = {
+    chart: {
+        type: 'bar',
+        height: 200
+    },
+    series: [{
+        name: '전월 자산 수',
+        data: [lastMonthDataValue_hotfix]
+    }, {
+        name: '현재 자산 수',
+        data: [currentDataValue_hotfix]
+    }],
+    xaxis: {
+        categories: ['hotfix']
+    }
+}
+var chart_hotfix = new ApexCharts(document.querySelector("#chart_hotfix"), options_hotfix);
+chart_hotfix.render();
+
+// 월간 자산 수 비교
+var dataFor_Notebook = dataList['Notebook_chassis_total'];
+var currentDataValue_Notebook = parseValue(dataFor_Notebook['current_value']);
+var lastMonthDataValue_Notebook = parseValue(dataFor_Notebook['last_value_in_prev_month']);
+var dataFor_Desktop = dataList['Desktop_chassis_total'];
+var currentDataValue_Desktop = parseValue(dataFor_Desktop['current_value']);
+var lastMonthDataValue_Desktop = parseValue(dataFor_Desktop['last_value_in_prev_month']);
+
+var options_chassis = {
+    chart: {
+        type: 'bar',
+        height: 250,
+        stacked: false
+    },
+    plotOptions: {
+        bar: {
+            horizontal: false,
+            columnWidth: '50%',
+            dataLabels: {
+                position: 'top', // top, center, bottom
+            },
+        },
+    },
+    series: [{
+        name: '전월 자산 수',
+        data: [lastMonthDataValue_Notebook, lastMonthDataValue_Desktop]
+    }, {
+        name: '현재 자산 수',
+        data: [currentDataValue_Notebook, currentDataValue_Desktop]
+    }],
+    xaxis: {
+        categories: ['노트북', '데스크탑']
+    }
+}
+
+var chart_chassis = new ApexCharts(document.querySelector("#chart_chassis"), options_chassis);
+chart_chassis.render();
+
+// Window 버전별 데이터를 테이블로 삽입
+var winVerData = dataList['win_ver'];
+var tableBody = document.getElementById('winVerTableBody');
+
+for (var version in winVerData) {
+    var row = tableBody.insertRow();
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    cell1.textContent = version;
+    cell2.textContent = winVerData[version];
+}v
+
+
 
 
 
